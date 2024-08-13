1f5a0228f9028964c8b9956ff6b55261e6a87467
InuYashahime <114967356+InuYashahime@users.noreply.github.com>
2024-08-13 18:19:58 +0300

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ global.prems = []
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
-global.botNumberCode = "+994406674437" //Ejemplo: +59309090909
+global.botNumberCode = "+994405372036" //Ejemplo: +59309090909
 global.confirmCode = "" 
 
 //---------[ IDIOMA : LENGUAJE ]---------
